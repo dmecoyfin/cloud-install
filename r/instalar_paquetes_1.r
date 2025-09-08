9cd31b368d374e4114a1c7fbd7d3f1c68518d67e
GustavoDenicolay <gustavo.denicolay@gmail.com>
2025-09-08 17:27:12 -0300

    insta

--- r/instalar_paquetes_1.r
+++ r/instalar_paquetes_1.r
@@ -3,7 +3,16 @@ options(Ncpus = 4)
 
 install.packages( "pak",  dependencies= TRUE, INSTALL_opts="--no-multiarch" )
 require("pak")
-pak::pkg_install( "mlflow" )
+
+require("bspm")
+bspm::disable()
+paq1 <- c("data.table", "rpart", "yaml", "httr", "devtools", "yaml", "rlist")
+paq2 <- c("magrittr", "stringi", "curl", "openssl", "roxygen2", "ranger")
+paq3 <- c("dplyr", "caret", "covr", "lintr", "tidyverse", "tidyr", "shiny")
+pag4 <- c("ggplot2", "plotly", "mlflow")
+
+paquetes <-  c( paq1, paq2, paq3, paq4, paq5 )
+pak::pkg_install( paquetes )
 
 
 library( "devtools" )
